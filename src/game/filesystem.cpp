ahockersten/liero

Assume Emscripten has Linux-like filesystem
It has something, which they say is basically drop-in, so this should do
it.

src/game/filesystem.cpp
@@ -152,7 +152,7 @@ struct filename_result
 	char const* name;
 };
 
-#if GVL_LINUX || __APPLE__
+#if GVL_LINUX || __APPLE__ || __EMSCRIPTEN__
 
 # define BOOST_HANDLE DIR *
 # define BOOST_INVALID_HANDLE_VALUE 0

openliero/openliero

src/game/filesystem.cpp
@@ -150,7 +150,7 @@ struct filename_result
 	char const* name;
 };
 
-#if __unix__ || __APPLE__
+#if __unix__ || __APPLE__ || __EMSCRIPTEN__
 
 # define BOOST_HANDLE DIR *
 # define BOOST_INVALID_HANDLE_VALUE 0