719b421446cbe2b0c45ea7a1a02793eacde36dbf
George Rawlinson <george@rawlinson.net.nz>
2024-09-10 14:51:27 +1200

    refactor: turn gvl into it's own library

---
+++ src/gvl/src/io2/convert.cpp
@@ -0,0 +1,9 @@
+#include "gvl/io2/convert.hpp"
+
+namespace gvl
+{
+
+uint8_t const no_caps[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+uint8_t const caps[]    = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+}
