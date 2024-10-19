openliero/openliero

commit b2f09c0432cb03a3e884a7d08ffea9bc1a7a952d

fix: some more cppcheck warnings

src/game/common.cpp
@@ -332,8 +332,7 @@ void Common::load(FsNode node) {
 
         s.originalData.resize(dataSize);
 
-        for (auto& z : s.originalData)
-          z = r.get() - 128;
+        std::fill(s.originalData.begin(), s.originalData.end(), r.get() - 128);
 
         s.sound = sfx_new_sound(dataSize * 2);
 

src/game/ai/predictive_ai.cpp
@@ -445,12 +445,7 @@ void evaluate(
   Worm* meCopy = copy.wormByIdx(me->index);
   Worm* targetCopy = copy.wormByIdx(target->index);
 
-  FollowAI* targetAi = 0;
-
   auto context = ai.currentContext;
-  InputContext targetContext;
-  if (targetAi)
-    targetContext = targetAi->currentContext;
 
   SimpleAI simpleAi;
 
@@ -519,11 +514,7 @@ void evaluate(
 
     meCopy->controlStates = context.update(plan[i], copy, meCopy, ai);
 
-    if (targetAi && targetAi->best && i < targetAi->best->plan.size())
-      targetCopy->controlStates = targetContext.update(
-          targetAi->best->plan[i], copy, targetCopy, *targetAi);
-    else
-      simpleAi.process(copy, *targetCopy);
+    simpleAi.process(copy, *targetCopy);
 
     copy.processFrame();
 

src/game/ai/predictive_ai.hpp
@@ -25,9 +25,8 @@ struct InputState {
 
     if (!cs[static_cast<int>(Worm::Control::Change)]) {
       // MoveJumpFire
-      idx = (v >> 2) << 1;
-      if (idx > 48)
-        idx -= (1 << 5);
+      // TODO check out `git blame` for this particular code if there are any
+      // issues. idx was assigned/overwritten a few times *prior*.
       idx = 2;
       idx |= (v & 1);
     } else {

src/game/common.hpp
@@ -58,13 +58,11 @@ struct ColourAnim {
 
 /* AI parameters sourced from [[constants.aiparams]] in tc.cfg */
 struct AIParams {
-  int k[NUM_AIPARAMS_VALUES][NUM_AIPARAMS_KEYS];  // 0x1AEEE, contiguous words
+  // 0x1AEEE, contiguous words
+  int k[NUM_AIPARAMS_VALUES][NUM_AIPARAMS_KEYS];
 };
 
 struct SfxSample : gvl::noncopyable {
-  // SfxSample(SfxSample const&) = delete;
-  // SfxSample& operator=(SfxSample const&) = delete;
-
   SfxSample() : sound(0) {}
 
   SfxSample(SfxSample&& other)
@@ -76,9 +74,11 @@ struct SfxSample : gvl::noncopyable {
 
   SfxSample& operator=(SfxSample&& other) {
     name = std::move(other.name);
-    sound = other.sound;
-    sound = 0;
     originalData = std::move(other.originalData);
+    // TODO the below code was originally zero'd out could be a bug or
+    // something. noted for future reference.
+    sound = other.sound;
+
     return *this;
   }
 

src/game/filesystem.cpp
@@ -428,10 +428,11 @@ struct FsNodeZipFile : FsNodeImp {
           std::string const& part = filepath.substr(beg, i - beg);
 
           auto& c = cur->children[part];
-          if (!c)
+          if (!c) {
             c.reset(new FsNodeZipFile(
                 archive, joinPath(cur->path, part),
                 joinPath(cur->relPath, part), -1, true));
+          }
           cur = c.get();
 
           beg = i + 1;
@@ -549,7 +550,7 @@ struct FsNodeFilesystem : FsNodeImp {
   bool exists() const override { return access(path.c_str(), 0) != -1; }
 
   gvl::source tryToSource() override {
-    FILE* f = tolerantFOpen(path.c_str(), "rb");
+    FILE* f = tolerantFOpen(path, "rb");
     if (!f)
       return gvl::source();
 

src/game/game.cpp
@@ -477,24 +477,26 @@ void Game::startGame() {
 }
 
 bool Game::isGameOver() {
-  if (settings->gameMode == Settings::GameMode::KillEmAll ||
-      settings->gameMode == Settings::GameMode::ScalesOfJustice) {
-    for (std::size_t i = 0; i < worms.size(); ++i) {
-      if (worms[i]->lives <= 0)
-        return true;
-    }
-  } else if (settings->gameMode == Settings::GameMode::GameOfTag) {
-    for (std::size_t i = 0; i < worms.size(); ++i) {
-      if (worms[i]->timer >= settings->timeToLose)
-        return true;
-    }
-  } else if (settings->gameMode == Settings::GameMode::Holdazone) {
-    for (const auto* w : worms)
-      if (w->timer >= settings->timeToLose)
-        return true;
+  int timeToLose = settings->timeToLose;
+  switch (settings->gameMode) {
+    case Settings::GameMode::KillEmAll:
+    // [[fallthrough]]; C++17
+    case Settings::GameMode::ScalesOfJustice:
+      return std::any_of(worms.begin(), worms.end(), [](const Worm* w) {
+        return w->lives <= 0;
+      });
+      break;
+    case Settings::GameMode::GameOfTag:
+    // [[fallthrough]]; C++17
+    case Settings::GameMode::Holdazone:
+      return std::any_of(
+          worms.begin(), worms.end(),
+          [timeToLose](const Worm* w) { return w->timer >= timeToLose; });
+      break;
+    default:
+      return false;
+      break;
   }
-
-  return false;
 }
 
 void Game::doDamageDirect(Worm& w, int amount, int byIdx) {

src/game/settings.hpp
@@ -108,10 +108,10 @@ void archive_liero(Archive ar, Settings& settings) {
       .b(settings.regenerateLevel)
       .b(settings.shadow);
 
-  if (ar.in)
+  if (ar.in) {
     settings.wormSettings[0]->controller %= 3;
-  if (ar.in)
     settings.wormSettings[1]->controller %= 3;
+  }
 
   for (int i = 0; i < 40; ++i) {
     ar.ui8(settings.weapTable[i]);

src/game/spectatorviewport.cpp
@@ -370,7 +370,7 @@ void SpectatorViewport::draw(
     {
       if (i->curFrame == 0) {
         int nameNum =
-            static_cast<int>(&*i - game.wobjects.arr) %
+            static_cast<int>(i - game.wobjects.arr) %
             static_cast<int>(
                 common.weapons.size());  // TODO: Something nicer maybe
 

commit c4154a8e8fb784fc86d6c605c5c4943c41593312

fix: sound regression
Fixes regression introduced in b2f09c0432cb03a3e884a7d08ffea9bc1a7a952d
with std::fill.

src/game/common.cpp
@@ -333,7 +333,9 @@ void Common::load(FsNode node) {
 
         s.originalData.resize(dataSize);
 
-        std::fill(s.originalData.begin(), s.originalData.end(), r.get() - 128);
+        // replacing below loop with std::fill is a regression
+        for (auto& z : s.originalData)
+          z = r.get() - 128;
 
         s.sound = sfx_new_sound(dataSize * 2);
 